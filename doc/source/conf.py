49369eadbfbdc8ecf10c07d0e73642a1e4725509
Vladimir Vechkanov <vvechkanov@miramtis.com>
2013-03-25 14:44:22 +0400

    Change sphinx configuration to support UML diagram
    Change-Id: Ib5185ddd07033916d83f453b165cf10f90671fdf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,9 @@ sys.path.extend([os.path.abspath('../swift'), os.path.abspath('..'),
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
               'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
-              'sphinx.ext.ifconfig']
+              'sphinx.ext.ifconfig', 'sphinxcontrib.plantuml']
 todo_include_todos = True
+plantuml = ['java','-jar','/home/swift/java/plantuml.jar']
 
 # Add any paths that contain templates here, relative to this directory.
 # Changing the path so that the Hudson build output contains GA code and the
